3e3d66b84f3220b9fea5d15666e468deb1a28148
Jian Li <jianli@njupt.edu.cn>
2019-07-05 08:46:30 +0800

    MDI version
    Add a new version for measurement-device-independent tomography

---
+++ fcostB.m
@@ -0,0 +1,18 @@
+%
+function fcst = fcostB(par);
+global n_exp;
+global n_tot;
+global PP;
+
+t = par;
+rho = T2R(t);
+n = length(PP);
+n_est = n_exp*0;
+for i = 1:n;
+    n_est(i) = trace(rho*PP{i})*n_tot;
+end
+n_sig = (n_est>0);
+n_fr = n_est.*(1+n_sig)/2+(1-n_sig);
+
+fcst = sum((n_exp-n_est).^2./n_fr);
+
